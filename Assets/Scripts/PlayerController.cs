3256eb716ba1d5ca0bca00719bf81ac12a48677c
Mohan Kumar <btmohankumar3212@gmail.com>
2021-07-21 16:51:35 +0530

    Player Movement Horizontal

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,41 +5,57 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public Animator animator;
+    public float speed;
 
     private void Update()
     {
-        
-        float speed = Input.GetAxisRaw("Horizontal");
-        animator.SetFloat("Speed", Mathf.Abs(speed));
-        Vector3 scale = transform.localScale;
-        if (speed< 0)
-        {
-            scale.x = -1f * Mathf.Abs(scale.x);
-        }else if(speed > 0)
-        {
-            scale.x = Mathf.Abs(scale.x);
-        }
-        transform.localScale = scale;
+
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        MoveCharacter(horizontal);
+        PlayMovementAnimation(horizontal);
 
         // Code to make crouch animation 
-        if(Input.GetKeyDown(KeyCode.LeftControl))
+        if (Input.GetKeyDown(KeyCode.LeftControl))
         {
             animator.SetBool("isCrouch", true);
-        }else if(Input.GetKeyUp(KeyCode.LeftControl))
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftControl))
         {
             animator.SetBool("isCrouch", false);
         }
 
         // Code to make jump animation
         //float jump = Input.GetAxisRaw("Jump");
-                                                                             if (Input.GetKeyDown(KeyCode.Space))
-      //if (jump > 0)
+        if (Input.GetKeyDown(KeyCode.Space))
+        //if (jump > 0)
         {
             animator.SetBool("isJump", true);
-        }else                                                                if (Input.GetKeyUp(KeyCode.Space))
+        }
+        else if (Input.GetKeyUp(KeyCode.Space))
         {
-           animator.SetBool("isJump", false);
+            animator.SetBool("isJump", false);
         }
 
+    }
+    private void MoveCharacter(float horizontal)
+    {
+        Vector3 position = transform.position;
+        position.x = position.x + horizontal * speed * Time.deltaTime;
+        transform.position = position;
+    }
+
+    private void PlayMovementAnimation(float horizontal)
+    {
+        animator.SetFloat("Speed", Mathf.Abs(horizontal));
+        Vector3 scale = transform.localScale;
+        if (horizontal < 0)
+        {
+            scale.x = -1f * Mathf.Abs(scale.x);
+        }
+        else if (horizontal > 0)
+        {
+            scale.x = Mathf.Abs(scale.x);
+        }
+        transform.localScale = scale;
     }
 }
